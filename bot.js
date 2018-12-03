27c86b0b7912a525ff2e14058ae63b51c3a5560a
Vonsek <45567075+Vonsek@users.noreply.github.com>
2018-12-03 16:04:21 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+const bot = new Discord.Client();
+
+var token = ("NTE4ODc0MzQyMzYxMTM3Mjcy.DuXHGA.0-InFKXYn1gkzHlD5zayQKJ1QDY");
+
+const fs = require("fs");
+
+const prefix = "//"
+
+bot.login(token);
